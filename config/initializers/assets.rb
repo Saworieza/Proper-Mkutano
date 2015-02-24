8d92e9ae936ee2adfd4930eb20dfec1296dcb57a
Saworieza <saworieza@gmail.com>
2015-02-24 13:04:19 +0300

    a redesign of the mkutano app made properly

---
+++ config/initializers/assets.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+Rails.application.config.assets.precompile += %w( application-145ebbae97fba5f8c6e03b2e3dbe19cb.css )
+Rails.application.config.assets.precompile += %w( application-1ae8502203dce617f805c5b6be7831ef.js )
