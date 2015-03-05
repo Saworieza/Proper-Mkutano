a920503f607c7696dd394f77ad0a5c52d4f1fce0
Saworieza <saworieza@gmail.com>
2015-03-05 10:57:03 +0300

    put the calendar in but the moduus sign is confusing the RoR processor into thinking its ruby code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,13 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 Rails.application.config.assets.precompile += %w( application-145ebbae97fba5f8c6e03b2e3dbe19cb.css )
+Rails.application.config.assets.precompile += %w( clndr.css )
+
 Rails.application.config.assets.precompile += %w( application-1ae8502203dce617f805c5b6be7831ef.js )
+Rails.application.config.assets.precompile += %w( json2.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( underscore-min.js )
+Rails.application.config.assets.precompile += %w( moment-2.8.3.js )
+Rails.application.config.assets.precompile += %w( moment-2.8.3.js )
+Rails.application.config.assets.precompile += %w( clndr.js )
+Rails.application.config.assets.precompile += %w( test.js )
