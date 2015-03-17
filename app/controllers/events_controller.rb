4c22979f8c51daeebb043339ba0d005af95ee85a
Saworieza <saworieza@gmail.com>
2015-03-17 14:10:17 +0300

    fixed the sunspot pagination - reversed the order to start from the more recent events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,12 +11,12 @@ class EventsController < ApplicationController
     elsif params.has_key?("location")
             @events = Event.where(location: params[:location]).order("created_at DESC").paginate(page: params[:page], per_page: 10)
     else
-      @events = Event.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)#y you no work?
+      @events = Event.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
       
       @query = Event.search do
           fulltext params[:search]
       end
-      @events = Event.where(id: @query.results.map(&:id)).paginate(page: params[:page], per_page: 3)
+      @events = Event.where(id: @query.results.map(&:id)).order("created_at DESC").paginate(page: params[:page], per_page: 10)
     end
   end
 
