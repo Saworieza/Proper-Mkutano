636761432f70f85d6521c928c1907679434ef582
Saworieza <saworieza@gmail.com>
2015-03-19 16:33:27 +0300

    minor changes to routes and sessions new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   as :user do
     get "/login" => "devise/sessions#new"
-  
     delete "/logout" => "devise/sessions#destroy"
     get "/signup" => "devise/registrations#new"
   end
